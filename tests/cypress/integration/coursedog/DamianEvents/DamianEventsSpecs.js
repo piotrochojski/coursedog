30094e13743d5f7f578494a57a0df322903bdf82
Piotr Ochojski <piotrochojski@MacBook-Pro-6.local>
2021-09-28 14:26:29 +0200

    freamework and scenarios from excercise doc

---
+++ tests/cypress/integration/coursedog/DamianEvents/DamianEventsSpecs.js
@@ -0,0 +1,75 @@
+import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
+import { damianEventsLandingPage, eventDetailsCard } from '../../../support/pages/coursedogEvents.po'
+
+const damianEventsLanding = new damianEventsLandingPage()
+const eventDetails = new eventDetailsCard()
+
+Given('Damian Events page is opened', () => {
+    cy.visit('/')
+    damianEventsLanding.upcoming_events_header().should('be.visible')
+});
+
+Given('todays date is set to {string}', (date) => {
+    const dateArray = date.split("-")
+    const given_todays_date = new Date(dateArray[0], dateArray[1] - 1, dateArray[2]).getTime()
+    cy.clock(given_todays_date)
+})
+
+When('user gets events for date {string}', (date) => {
+    const dateUrl = date.replaceAll('-', '/')
+    cy.visit(`${dateUrl}`)
+  });
+
+When('user clicks on Todays Events button', () => {
+    cy.intercept('GET', '**/meetings?**').as('meetings_request')
+    damianEventsLanding.todays_events_button().click();
+    cy.wait('@meetings_request')
+    cy.wait(500)
+})
+
+When('user clicks on Featured Events button', () => {
+    cy.intercept('GET', '**/meetings?**').as('meetings_request')
+    damianEventsLanding.featured_events_button().click();
+    cy.wait('@meetings_request')
+})
+
+Then('user clicks event card name {string}', (event_name) => {
+    damianEventsLanding.event_content().contains(event_name).click()
+})
+
+Then('no events shown with message {string}', (message) => {
+    damianEventsLanding.no_upcoming_events().contains(message)
+    damianEventsLanding.event_content().should('not.exist')
+})
+
+Then('event named {string} is shown', (event_name) => {
+    damianEventsLanding.event_content().contains(event_name)
+})
+
+Then('there are exactly {int} events shown on the page', (events_counter) => {
+    damianEventsLanding.event_content().should('have.length', events_counter)
+})
+
+Then('button add to calendar is shown', () => {
+    eventDetails.add_to_calendar_link().should('be.visible')
+})
+
+Then('button add to Google Calendar is shown', () => {
+    eventDetails.add_to_google_calendar_link().should('be.visible')
+})
+
+Then('event type details are shown', () => {
+    eventDetails.event_type_details().should('be.visible')
+})
+
+Then('organized by is shown', () => {
+    eventDetails.organization_event_details().should('be.visible')
+})
+
+Then('contacts is shown', () => {
+    eventDetails.contacts_event_details().should('be.visible')
+})
+
+Then('event description details are shown', () => {
+    eventDetails.event_description_details().should('be.visible')
+})
---
+++ tests/cypress/support/pages/coursedogEvents.po.js
@@ -0,0 +1,71 @@
+// landing page
+
+const upcoming_events_header = '.mb-4 > .flex'
+const date_on_a_calendar = '*[class^="vc-day id-REPLACE_WITH_DATE"]'
+const all_days_calendar_grid = '*[class^="vc-grid-container vc-weeks"]'
+const calendar_month_back = ':nth-child(1) > .vc-svg-icon'
+const calendar_month_forward = ':nth-child(2) > .vc-svg-icon'
+const no_upcoming_events = '.mt-16 > .mt-8'
+const events_grid = 'section'
+const event_content = '.card-content'
+const todays_events_button = '[href="/today"]'
+const featured_events_button = '[href="/featured"]'
+const add_to_calendar_link = '.mt-2 > .mr-2'
+const add_to_google_calendar_link = 'a > .btn'
+const event_type_details = '.items-start > .font-semibold'
+const organization_event_details = ':nth-child(6) > .font-semibold'
+const contacts_event_details = '.cursor-pointer'
+const event_description_details = '.text-theme-darkest'
+
+
+class damianEventsLandingPage {
+    upcoming_events_header() {
+        return cy.get(upcoming_events_header)
+    }
+    no_upcoming_events() {
+        return cy.get(no_upcoming_events)
+    }
+    date_on_a_calendar(date) {
+        return cy.get(date_on_a_calendar.replace('REPLACE_WITH_DATE', `${date}`))
+    }
+    events_grid() {
+        return cy.get(events_grid)
+    }
+    event_content() {
+        return cy.get(event_content)
+    }
+    todays_events_button() {
+        return cy.get(todays_events_button)
+    }
+    featured_events_button() {
+        return cy.get(featured_events_button)
+    }
+}
+
+class eventDetailsCard {
+    add_to_calendar_link() {
+        return cy.get(add_to_calendar_link)
+    }
+    add_to_google_calendar_link() {
+        return cy.get(add_to_google_calendar_link)
+    }
+    event_type_details() {
+        return cy.get(event_type_details)
+    }
+    organization_event_details() {
+        return cy.get(organization_event_details)
+    }
+    contacts_event_details() {
+        return cy.get(contacts_event_details)
+    }
+    event_description_details() {
+        return cy.get(event_description_details)
+    }
+}
+
+
+
+export {
+    damianEventsLandingPage,
+    eventDetailsCard
+}
